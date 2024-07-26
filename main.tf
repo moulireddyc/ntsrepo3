be1139ff87fc8e2b33ee61ab5e97ef08bc307ae7
Mouliswara Reddy <moulireddyc@gmail.com>
2024-07-26 10:51:20 +0100

    Update main.tf
    c4

--- main.tf
+++ main.tf
@@ -9,3 +9,10 @@ resource "aws_vpc" "dep1" {
       "Name" = "devenv-${count.index}"    
     }  
 }
+resource "aws_vpc" "dep2" {
+    count = 1
+    cidr_block = "10.1.0.0/16"
+    tags = {
+      "Name" = "prodenv-${count.index}"    
+    }  
+}
